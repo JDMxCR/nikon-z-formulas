684d4fbe2b780aeaea0e28661b5720a43e4dd6ca
JDRXcr <jj.duartem@gmail.com>
2024-07-13 22:17:10 +0200

    update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,16 @@
 import { SiNikon } from "react-icons/si";
 import { BiSolidUserCircle } from "react-icons/bi";
-import { Link } from "@nextui-org/react";
+import logo from "../assets/logo-w.png";
+
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center px-10 py-5 text-2xl font-medium text-white bg-gradient-to-br from-yellow-300 to-yellow-500 min-w-80">
-      <Link href="/" passHref>
-        <SiNikon className="sm:size-16 size-10" />
-      </Link>
+      <SiNikon className="sm:size-16 size-10" />
       <div>
-        <img
-          className="h-16"
-          src=".\src\assets\logo-w.png"
-          alt="z formulas logo"
-        />
+        <img className="h-16" src={logo} alt="z formulas logo" />
       </div>
       <div>
-        <Link href="/login" passHref>
-          <BiSolidUserCircle className="sm:size-8 rise-shake" />
-        </Link>
+        <BiSolidUserCircle className="sm:size-8 rise-shake" />
       </div>
     </nav>
   );
